e3238eaf3587a0eb7c5c51150afc1635639ade0e
Gornovaya <79137339+Gornovaya@users.noreply.github.com>
2021-02-25 12:58:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd (int a, int b) {
-   if (a == 0)
-      return b;
-   else
-      return gcd(a, b % a);
+ if (a == 0)
+  return b;
+ else
+  return gcd(a, b % a);
 }
 
